b579d60b7342f72522aa49d79e665198b6615850
dpente <pentedg1@student.op.ac.nz>
2021-09-29 15:33:58 +1300

    Added universal variables and threading

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        public static string inv, playerInput;
+        public static int playerLocation;
+        public static bool doorE3 = false, doorE13 = false, doorE16 = false, doorS17 = false, doorN14 = false, doorS20 = false, doorE74 = false;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
