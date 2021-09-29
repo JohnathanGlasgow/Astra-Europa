66724f79667901e2257aa5f59de562d7387b63ae
dpente <pentedg1@student.op.ac.nz>
2021-09-29 15:36:24 +1300

    Added room 1

--- Program.cs
+++ Program.cs
@@ -9,7 +9,35 @@ namespace ConsoleApp1
         public static int playerLocation;
         public static bool doorE3 = false, doorE13 = false, doorE16 = false, doorS17 = false, doorN14 = false, doorS20 = false, doorE74 = false;
 
-        static void Main(string[] args)
+		public static void room1()
+		{
+			while (playerInput != "south" || playerInput != "east")
+			{
+				Console.Clear();
+				Console.WriteLine("ROOM DESCRIPTION 1");
+				playerInput = Console.ReadLine();
+				switch (playerInput)
+				{
+					case "south":
+						room2();
+						break;
+					case "east":
+						room6();
+						break;
+					default:
+						Console.WriteLine("Invaild Input");
+						Thread.Sleep(500);
+						break;
+				}
+			}
+		}
+
+
+
+
+
+
+		static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
